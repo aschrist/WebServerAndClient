0da56cd50b76d8d5d4ca63d63ff2ce7dfa0cae75
den <den@Dens-MBP.attlocal.net>
2018-10-23 01:31:00 -0700

    solve patient issue on dispach page

--- static/js/dispatch.js
+++ static/js/dispatch.js
@@ -370,6 +370,18 @@ function dispatchCall() {
                 obj['age'] = parseInt(patient[1]);
             patients.push(obj);
         }
+    if (!currentPatients) {
+        var name = $('#patient-' + index + '-name').val().trim();
+        var age = $('#patient-' + index + '-age').val().trim();
+        // is name empty?
+        if (!name) {
+            bsalert('Empty name');
+            return;
+        }
+        // add patient
+        var patient = [name, age];
+        patients.push({'name': name, 'age':parseInt(age)});
+    }
     form['patient_set'] = patients;
 
     // make json call
